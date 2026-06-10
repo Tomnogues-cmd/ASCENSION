0abe178545e2f8719eb6f33b660384536ff26604
Tom Nogues <tom@ascension.app>
2026-06-10 16:10:36 +0200

    Launch ASCENSION v1.0 - gamified screen time app

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 420,
+    height: 820,
+    minWidth: 380,
+    minHeight: 700,
+    title: 'ASCENSION',
+    backgroundColor: '#0a0a0f',
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, 'icon.ico'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    show: false,
+  });
+
+  win.loadFile('index.html');
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+
+  // Open external links in browser
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
